5e6f0926732e3614ee84db4e0316d65f58901fed
vinopermana77 <vinopermana13@gmail.com>
2023-03-21 20:21:07 +0700

    Update pas foto

--- js/script.js
+++ js/script.js
@@ -1,82 +1,82 @@
 // Sticky Navbar
-let header = document.querySelector('header');
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
-window.addEventListener('scroll', () => {
-  header.classList.toggle('shadow', window.scrollY > 0);
+window.addEventListener("scroll", () => {
+ header.classList.toggle("shadow", window.scrollY > 0);
 });
 
 menu.onclick = () => {
-  navbar.classList.toggle('active');
+ navbar.classList.toggle("active");
 };
 window.onscroll = () => {
-  navbar.classList.remove('active');
+ navbar.classList.remove("active");
 };
 
 // Dark Mode
-let darkmode = document.querySelector('#darkmode');
+let darkmode = document.querySelector("#darkmode");
 
 darkmode.onclick = () => {
-  if (darkmode.classList.contains('bx-moon')) {
-    darkmode.classList.replace('bx-moon', 'bx-sun');
-    document.body.classList.add('active');
-  } else {
-    darkmode.classList.replace('bx-sun', 'bx-moon');
-    document.body.classList.remove('active');
-  }
+ if (darkmode.classList.contains("bx-moon")) {
+  darkmode.classList.replace("bx-moon", "bx-sun");
+  document.body.classList.add("active");
+ } else {
+  darkmode.classList.replace("bx-sun", "bx-moon");
+  document.body.classList.remove("active");
+ }
 };
 
 // Slider
-var swiper = new Swiper('.slide-content', {
-  slidesPerView: 3,
-  spaceBetween: 25,
-  loop: false,
-  centerSlide: 'true',
-  fade: 'true',
-  grabCursor: 'true',
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    dynamicBullets: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+var swiper = new Swiper(".slide-content", {
+ slidesPerView: 3,
+ spaceBetween: 25,
+ loop: false,
+ centerSlide: "true",
+ fade: "true",
+ grabCursor: "true",
+ pagination: {
+  el: ".swiper-pagination",
+  clickable: true,
+  dynamicBullets: true,
+ },
+ navigation: {
+  nextEl: ".swiper-button-next",
+  prevEl: ".swiper-button-prev",
+ },
 
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    520: {
-      slidesPerView: 2,
-    },
-    950: {
-      slidesPerView: 3,
-    },
+ breakpoints: {
+  0: {
+   slidesPerView: 1,
   },
+  520: {
+   slidesPerView: 2,
+  },
+  950: {
+   slidesPerView: 3,
+  },
+ },
 });
 
 // Contact
 function sendEmail() {
-  var params = {
-    name: document.getElementById('name').value,
-    email: document.getElementById('email').value,
-    message: document.getElementById('message').value,
-  };
+ var params = {
+  name: document.getElementById("name").value,
+  email: document.getElementById("email").value,
+  message: document.getElementById("message").value,
+ };
 
-  const serviceID = 'service_7vtce3y';
-  const templateID = 'template_9jstit8';
+ const serviceID = "service_7vtce3y";
+ const templateID = "template_9jstit8";
 
-  emailjs
-    .send(serviceID, templateID, params)
-    .then((res) => {
-      document.getElementById('name').value = '';
-      document.getElementById('email').value = '';
-      document.getElementById('message').value = '';
-      console.log(res);
-      alert('Your message sent successfully!!');
-    })
-    .catch((err) => console.log(err));
+ emailjs
+  .send(serviceID, templateID, params)
+  .then((res) => {
+   document.getElementById("name").value = "";
+   document.getElementById("email").value = "";
+   document.getElementById("message").value = "";
+   console.log(res);
+   alert("Your message sent successfully!!");
+  })
+  .catch((err) => console.log(err));
 }
